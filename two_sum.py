658e871742bc5604011cba8584229dceb0cc6862
Prasad Patil <patil.prasad76@gmail.com>
2023-12-06 13:46:58 -0800

    linear time solution for two sum problem

--- two_sum.py
+++ two_sum.py
@@ -1,8 +1,19 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        map = {}
+        for i, value in enumerate(nums):
+            remainder = target - value
+            if remainder in map:
+                return [i, map[remainder]]
+            map[value] = i
+
+
+""" class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
         size = len(nums)
         for i in range(size):
             # j will start after i beacuse we can't use same number twice
             for j in range(i+1, size):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+ """
